78d537c232093706b9f005581ff92b87068b0d2d
Jeremy Moseley <jeremymoeley@me.com>
2018-05-30 07:55:51 -0700

    Slideshow and block content addition

--- index.php
+++ index.php
@@ -1,29 +1,79 @@
 <?php get_header(); ?>
+
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+
+      <?php if ( ! is_front_page() ) : ?>
+
+        <div class="title">
+          <div class="row">
+            <div class="col text-center">
+              <h1><?php the_title(); ?></h1>
+            </div>
+          </div>
+        </div>
+
+      <?php endif; ?>
+
       <?php if ( have_rows('blocks') ) : ?>
+
         <?php while ( have_rows('blocks') ) : the_row(); ?>
-          <?php if ( get_row_layout() == 'photo' ) : ?>
-            <?php $photo = get_sub_field('photo'); ?>
-            <?php if ( $photo ) : ?>
+
+          <?php if ( get_row_layout() == 'photos' ) : ?>
+
+            <?php $photos = get_sub_field('photos'); ?>
+
+            <?php if ( $photos ) : ?>
+              <?php
+
+              if (get_sub_field('gallery_type') == 'full') {
+                $slickOptions = '{"dots": true}';
+              }
+              elseif (get_sub_field('gallery_type') == 'two') {
+                $slickOptions = '{"slidesToShow": 2, "slidesToScroll": 1}';
+              }
+              ?>
+
               <div class="row">
                 <div class="col">
-                  <div class="block-photo">
-                    <img src="<?php echo $photo['url']; ?>" class="img-fluid">
-                    <?php if ( get_sub_field('overlay') != 'na' ) : ?>
-                      <div class="caption-<?php the_sub_field('overlay'); ?> caption-pos-<?php the_sub_field('position'); ?>">
-                        <?php if ( $photo['title'] != '' ) : ?>
-                          <h3><?php echo $photo['title']; ?></h3>
+                  <div class="slick two-gallery" data-slick='<?php echo $slickOptions; ?>'>
+
+                    <?php foreach ($photos as $photo) : ?>
+
+                      <div class="block-photo">
+                        <img src="<?php echo $photo['url']; ?>" class="img-fluid">
+
+                        <?php if ( get_sub_field('overlay') != 'na' ) : ?>
+
+                          <div class="caption-<?php the_sub_field('overlay'); ?> caption-pos-<?php the_sub_field('position'); ?>">
+
+                            <?php if ( $photo['title'] != '' ) : ?>
+
+                              <h3><?php echo $photo['title']; ?></h3>
+
+                            <?php endif; ?>
+
+                            <?php if ( $photo['caption'] != '' ) : ?>
+
+                              <p><?php echo $photo['caption']; ?></p>
+
+                            <?php endif; ?>
+
+                          </div>
+
                         <?php endif; ?>
-                        <?php if ( $photo['caption'] != '' ) : ?>
-                          <p><?php echo $photo['caption']; ?></p>
-                        <?php endif; ?>
+
                       </div>
-                    <?php endif; ?>
+
+                    <?php endforeach; ?>
+
                   </div>
                 </div>
               </div>
+
             <?php endif; ?>
+
           <?php elseif ( get_row_layout() == 'headline' ) : ?>
+
             <div class="row">
               <div class="col">
                 <div class="block-headline">
@@ -32,32 +82,31 @@
                 </div>
               </div>
             </div>
-          <?php elseif ( get_row_layout() == 'gallery' ) : ?>
-            <?php $gallery = get_sub_field('gallery'); ?>
-            <?php if ( $gallery ) : ?>
+
+          <?php elseif ( get_row_layout() == 'content' ) : ?>
+
+            <div class="block-content block-content-<?php the_sub_field('background'); ?>">
               <div class="row">
-              <?php foreach ( $gallery as $image ) : ?>
-                <div class="col">
-                  <div class="block-photo">
-                    <img src="<?php echo $image['url']; ?>" class="img-fluid">
-                    <div class="caption">
-                      <?php if ( $image['title'] != '' ) : ?>
-                        <h3><?php echo $image['title']; ?></h3>
-                      <?php endif; ?>
-                      <?php if ( $image['caption'] != '' ) : ?>
-                        <p><?php echo $image['caption']; ?></p>
-                      <?php endif; ?>
-                    </div>
-                  </div>
-                </div>
-              <?php endforeach; ?>
+                <div class="col"><?php the_sub_field('content_1'); ?></div>
+                <?php if ( get_sub_field('columns') != '1' ) : ?>
+                  <div class="col"><?php the_sub_field('content_2'); ?></div>
+                <?php endif; ?>
+                <?php if ( get_sub_field('columns') == '3' ) : ?>
+                  <div class="col"><?php the_sub_field('content_3'); ?></div>
+                <?php endif; ?>
+              </div>
             </div>
-            <?php endif; ?>
+
           <?php endif; ?>
+
         <?php endwhile; ?>
+
       <?php else : ?>
-        <h1><?php the_title(); ?></h1>
+
         <?php the_content(); ?>
+
       <?php endif; ?>
+
     <?php endwhile; endif; ?>
+
 <?php get_footer(); ?>
