095aa341d13cb318651d138b679ed3fbbed049a2
Jeremy Moseley <jeremymoeley@me.com>
2018-06-10 17:21:13 -0700

    Footer template part update

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 <?php get_template_part( 'template-parts/form/contact', 'panel' ); ?>
 <footer class="footer">
   <div class="container">
-    <?php get_template_part( 'template-parts/footer', 'widgets' ); ?>
+    <?php get_template_part( 'template-parts/footer/widgets' ); ?>
   </div><!-- .container -->
 </footer>
 <?php wp_footer(); ?>
