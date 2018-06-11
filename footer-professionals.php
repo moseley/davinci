8d85b04f5ccf937c9070553d3def416d3b230802
Jeremy Moseley <jeremymoeley@me.com>
2018-06-10 17:23:06 -0700

    Update footer part for professionals footer

--- footer-professionals.php
+++ footer-professionals.php
@@ -2,7 +2,7 @@
 <?php get_template_part( 'template-parts/form/bid', 'panel' ); ?>
 <footer class="footer">
   <div class="container">
-    <?php get_template_part( 'template-parts/footer', 'widgets' ); ?>
+    <?php get_template_part( 'template-parts/footer/widgets' ); ?>
   </div><!-- .container -->
 </footer>
 <?php wp_footer(); ?>
