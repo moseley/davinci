de23a3d5299179a3f7e1184a9c50ce1064f70626
Jeremy Moseley <jeremymoeley@me.com>
2018-07-11 04:46:33 -0700

    Share Button

--- template-parts/blocks/content.php
+++ template-parts/blocks/content.php
@@ -81,6 +81,15 @@
     <div class="bg-white">
       <div class="container-fluid">
         <div class="row">
+          <?php if (get_sub_field('share')) : ?>
+            <div class="col-1 order-last">
+              <div class="block-headline">
+                <a class="addthis_button_compact">
+                  <img src="<?php echo get_theme_file_uri( '/assets/svg/share.svg' ); ?>" width="50" height="50" alt="Share">
+                </a>
+              </div>
+            </div>
+          <?php endif; ?>
           <div class="col">
             <div class="block-headline">
               <h2><?php the_sub_field('headline'); ?></h2>
