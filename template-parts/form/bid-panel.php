ff3f6472d0ab82872dd5df7415cb8b6872f60c83
Jeremy Moseley <jeremymoeley@me.com>
2018-07-12 01:23:12 -0700

    Balanced forms

--- template-parts/form/bid-panel.php
+++ template-parts/form/bid-panel.php
@@ -3,76 +3,63 @@
     <form action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="post" id="bid_form">
       <input type="hidden" name="action" value="bid_form_submission">
       <input type="hidden" name="bid_form_nonce" value="<?php echo $bid_form_nonce = wp_create_nonce( 'bid_form_nonce' ); ?>">
-    <div class="row align-items-center">
-
-        <div class="col-md-4">
+      <div class="row">
+        <div class="col text-center">
           <h2><?php _e('Bid Request', 'davinci'); ?></h2>
           <p><?php _e('DaVinci advisors are ready: ', 'davinci'); ?><strong><?php _e('800.654.1177', 'davinci'); ?></strong></p>
+          <div id="bid_response"></div>
         </div>
-        <div class="col-md-8">
-          <div class="row">
-            <div class="col-md">
-              <div id="bid_response"></div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md">
-              <div class="form-group">
-                <input type="text" class="form-control" id="first-name" name="bid[first_name]" placeholder="First name" required>
-              </div>
-            </div>
-            <div class="col-md">
-              <div class="form-group">
-                <input type="text" class="form-control" id="last-name" name="bid[last_name]" placeholder="Last name" required>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md">
-              <div class="form-group">
-                <input type="text" class="form-control" id="business-name" name="bid[business_name]" placeholder="Business Name" required>
-              </div>
-            </div>
-            <div class="col-md">
-              <div class="form-group">
-                <input type="email" class="form-control" id="email" name="bid[email]" placeholder="Email" required>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md">
-              <div class="form-group">
-                <input type="text" class="form-control" id="phone" name="bid[phone]" placeholder="Telephone" required>
-              </div>
-            </div>
-            <div class="col-md">
-              <div class="row">
-                <div class="col-9">
-                  <div class="form-group">
-                    <input type="text" class="form-control" id="city" name="bid[city]"placeholder="Job location: City" required>
-                  </div>
-                </div>
-                <div class="col-3">
-                  <div class="form-group">
-                    <input type="text" class="form-control" id="state" name="bid[state]" placeholder="State" required>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div><!-- .col-md-8 -->
-
       </div><!-- .row -->
-
+      <div class="row form-begin">
+        <div class="col-md">
+          <div class="form-group">
+            <input type="text" class="form-control" id="first-name" name="bid[first_name]" placeholder="First name" required>
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="form-group">
+            <input type="text" class="form-control" id="last-name" name="bid[last_name]" placeholder="Last name" required>
+          </div>
+        </div>
+      </div><!-- .row -->
       <div class="row">
-
-        <div class="col-md-4">
+        <div class="col-md">
+          <div class="form-group">
+            <input type="text" class="form-control" id="business-name" name="bid[business_name]" placeholder="Business Name" required>
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="form-group">
+            <input type="email" class="form-control" id="email" name="bid[email]" placeholder="Email" required>
+          </div>
+        </div>
+      </div><!-- .row -->
+      <div class="row">
+        <div class="col-md">
+          <div class="form-group">
+            <input type="text" class="form-control" id="phone" name="bid[phone]" placeholder="Telephone" required>
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="row">
+            <div class="col-sm-9">
+              <div class="form-group">
+                <input type="text" class="form-control" id="city" name="bid[city]"placeholder="Job location: City" required>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="form-group">
+                <input type="text" class="form-control" id="state" name="bid[state]" placeholder="State" required>
+              </div>
+            </div>
+          </div><!-- .row -->
+        </div>
+      </div><!-- .row -->
+      <div class="row">
+        <div class="col text-center">
           <button type="submit" class="btn btn-primary">Submit</button>
         </div>
-        <div class="col-md-8">&nbsp;</div>
-
       </div><!-- .row -->
-
     </form>
   </div>
 </div>
--- template-parts/form/contact-panel.php
+++ template-parts/form/contact-panel.php
@@ -3,72 +3,63 @@
     <form action="<?php echo esc_url( admin_url( 'admin-post.php' ) ); ?>" method="post" id="contact_form">
       <input type="hidden" name="action" value="contact_form_submission">
       <input type="hidden" name="contact_form_nonce" value="<?php echo $bid_form_nonce = wp_create_nonce( 'contact_form_nonce' ); ?>">
-    <div class="row align-items-center">
-
-        <div class="col-md-4">
+      <div class="row">
+        <div class="col text-center">
           <h2><?php _e('Intrigued by what you see?', 'davinci'); ?></h2>
           <p><?php _e('Allow a DaVinci advisor to tell you more.', 'davinci'); ?></p>
+          <div id="contact_response"></div>
         </div>
-        <div class="col-md-8">
-          <div class="row">
-            <div class="col-md">
-              <div id="contact_response"></div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md">
-              <div class="form-group">
-                <input type="text" class="form-control" id="first-name" name="contact[first_name]" placeholder="First name" required>
-              </div>
-            </div>
-            <div class="col-md">
-              <div class="form-group">
-                <input type="text" class="form-control" id="last-name" name="contact[last_name]" placeholder="Last name" required>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md">
-              <div class="form-group">
-                <input type="text" class="form-control" id="business-name" name="contact[business_name]" placeholder="Business Name" required>
-              </div>
-            </div>
-            <div class="col-md">
-              <div class="form-group">
-                <input type="email" class="form-control" id="email" name="contact[email]" placeholder="Email" required>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-md">
-              <div class="form-group">
-                <input type="text" class="form-control" id="phone" name="contact[phone]" placeholder="Telephone" required>
-              </div>
-            </div>
-            <div class="col-md">
-              <div class="row">
-                <div class="col-9">
-                  <div class="form-group">
-                    <input type="text" class="form-control" id="city" name="contact[city]" placeholder="Job location: City" required>
-                  </div>
-                </div>
-                <div class="col-3">
-                  <div class="form-group">
-                    <input type="text" class="form-control" id="state" name="contact[state]" placeholder="State" required>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-    </div><!-- .row -->
-    <div class="row">
-      <div class="col-md-4">
-        <button type="submit" class="btn btn-primary">Submit</button>
       </div>
-      <div class="col-md-8">&nbsp;</div>
-    </div>
+      <div class="row form-begin">
+        <div class="col-md">
+          <div class="form-group">
+            <input type="text" class="form-control" id="first-name" name="contact[first_name]" placeholder="First name" required>
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="form-group">
+            <input type="text" class="form-control" id="last-name" name="contact[last_name]" placeholder="Last name" required>
+          </div>
+        </div>
+      </div><!-- .row -->
+      <div class="row">
+        <div class="col-md">
+          <div class="form-group">
+            <input type="text" class="form-control" id="business-name" name="contact[business_name]" placeholder="Business Name" required>
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="form-group">
+            <input type="email" class="form-control" id="email" name="contact[email]" placeholder="Email" required>
+          </div>
+        </div>
+      </div><!-- .row -->
+      <div class="row">
+        <div class="col-md">
+          <div class="form-group">
+            <input type="text" class="form-control" id="phone" name="contact[phone]" placeholder="Telephone" required>
+          </div>
+        </div>
+        <div class="col-md">
+          <div class="row">
+            <div class="col-sm-9">
+              <div class="form-group">
+                <input type="text" class="form-control" id="city" name="contact[city]" placeholder="Job location: City" required>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="form-group">
+                <input type="text" class="form-control" id="state" name="contact[state]" placeholder="State" required>
+              </div>
+            </div>
+          </div><!-- .row -->
+        </div>
+      </div><!-- .row -->
+      <div class="row">
+        <div class="col text-center">
+          <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+      </div>
     </form>
   </div>
 </div>
